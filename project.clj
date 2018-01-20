c6cc62360b29850d873724ab950ae6a994d4eefc
EAS7510 <michaeleaster@northwesternmutual.com>
2018-01-19 22:36:21 -0600

    Version 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun.mike/cliff-alpha "0.0.4-SNAPSHOT"
+(defproject fun.mike/cliff-alpha "0.0.4"
   :description "CLI boilerplate."
   :url "https://github.com/mike706574/cliff"
   :license {:name "Eclipse Public License"
