1a68288fd79c72ffd9f56603bb242e7bbd92db08
EAS7510 <michaeleaster@northwesternmutual.com>
2018-01-19 21:45:22 -0600

    CLI boilerplate.

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject fun.mike/cliff "0.0.1-SNAPSHOT"
+  :description "CLI boilerplate."
+  :url "https://github.com/mike706574/cliff"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/tools.cli "0.3.5"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.stuartsierra/component "0.3.2"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :target-path "target/dev"
+                   :dependencies [[org.clojure/clojure "1.9.0"]
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [org.clojure/test.check "0.9.0"]]}}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]]
+  :repl-options {:init-ns user})
